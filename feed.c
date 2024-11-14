bbdf0060f8ffa09113cb013e9242a6e52135d25a
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-11-14 16:45:47 +0000

    Update feed.c

--- feed.c
+++ feed.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <sys/wait.h>
+#include <sys/types.h>
 #include <signal.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "feed.h"
 #define ManPipe "MANAGER_FIFO"
 #define FeedPipe "FEED_FIFO[%d]"
@@ -18,15 +20,29 @@ int is_invalid_command(const char *comando) {
            strcmp(comando, "topics") != 0 &&
            strcmp(comando, "unsubscribe") != 0 &&
            strcmp(comando, "exit") != 0;
+// verifica se o comando introduzido na consola é um comando válido
+}
+
+int is_invalid_login(const char *comando, const char *corpo){
+    return strcmp(comando,"login") == 0 && strcmp(corpo,"login");
 }
 
 
-int main()
+int main(int argc, char * argv[])
 {
     char comando[10],corpo[450];
     message msg;
     int fd_feed_fifo,sizeF;
     int fd_mngr_fifo,sizeM;
+    usr user;
+
+    strcpy(user.nome_utilizador,argv[1]);
+    if(strlen(user.nome_utilizador) > 20){
+        printf("\nNome demasiado grande, tente outra vez com um nome menor\n");
+        exit(1);
+    }
+    user.pid = getpid();
+    printf("%d, %s",user.pid, user.nome_utilizador);
 
     sprintf(feedpipe_final,"FEED_FIFO[%d]",getpid());
     if(mkfifo(feedpipe_final,0666) == -1){
@@ -37,18 +53,28 @@ int main()
         return 1;
     }
 
-    
+    fd_mngr_fifo = open (ManPipe,O_WRONLY);
+        if (fd_mngr_fifo == -1){
+            printf("Erro a abrir o pipe do manager, ou pipe nao existe\n");
+        return -1;
+        }
+        //fazer aqui averificação de possibilidade de adicionar utilizador no manager
+        //melhor fazer com sinais, 3 situações: sucesso - falha por nome igual - falha por max
+
     do{
+
         //lê inputs do user até ser introduzido um comando
         do {
             printf("\ninsira um comando valido:");
             fgets(corpo, sizeof(corpo) - 1, stdin);
             sscanf(corpo,"%s %[^\n]", comando, corpo);
         }while(is_invalid_command(comando));
-        //talvez criar isto numa função, menos clutter.
-        //comparar comando com os diferentes tipos de comando, se diferente pedir outra vez
         printf("\ncomando: %s \t resto:%s\n ", comando,corpo);
-        exit(1);
+        if(strcmp(comando,"exit") == 0){
+            unlink(feedpipe_final);
+            exit(1);
+        }
+
     }while(1);
 
 
