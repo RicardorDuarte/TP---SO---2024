a00dc064f7c56c23e4bed0a63c2fc9bcf27f9725
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-11-24 15:15:35 +0000

    Update feed.c

--- feed.c
+++ feed.c
@@ -15,7 +15,6 @@ char feedpipe_final[100];
 
 int is_invalid_command(const char *comando) {
     return strcmp(comando, "msg") != 0 &&
-           strcmp(comando, "login") != 0 &&
            strcmp(comando, "subscribe") != 0 &&
            strcmp(comando, "topics") != 0 &&
            strcmp(comando, "unsubscribe") != 0 &&
@@ -32,10 +31,12 @@ int main(int argc, char * argv[])
 {
     char comando[10],corpo[450];
     message msg;
-    int fd_feed_fifo,sizeF;
     int fd_mngr_fifo,sizeM;
     usr user;
-
+    if(!argv[1]){
+        printf("Nome nao introduzido, por favor introduza um nome.\n");
+        exit(1);
+    }
     strcpy(user.nome_utilizador,argv[1]);
     if(strlen(user.nome_utilizador) > 20){
         printf("\nNome demasiado grande, tente outra vez com um nome menor\n");
@@ -43,6 +44,8 @@ int main(int argc, char * argv[])
     }
     user.pid = getpid();
     printf("%d, %s\n",user.pid, user.nome_utilizador);
+    //fazer aqui averificação de possibilidade de adicionar utilizador no manager
+    //melhor fazer com sinais, 3 situações: sucesso - falha por nome igual - falha por max
 
     sprintf(feedpipe_final,"FEED_FIFO[%d]",getpid());
     if(mkfifo(feedpipe_final,0666) == -1){
@@ -59,8 +62,6 @@ int main(int argc, char * argv[])
             unlink(feedpipe_final);
             return -1;
         }
-        //fazer aqui averificação de possibilidade de adicionar utilizador no manager
-        //melhor fazer com sinais, 3 situações: sucesso - falha por nome igual - falha por max
 
     do{
 
@@ -70,12 +71,24 @@ int main(int argc, char * argv[])
             fgets(corpo, sizeof(corpo) - 1, stdin);
             sscanf(corpo,"%s %[^\n]", comando, corpo);
         }while(is_invalid_command(comando));
-        printf("\ncomando: %s \t resto:%s\n ", comando,corpo);
+
         if(strcmp(comando,"exit") == 0){
             unlink(feedpipe_final);
             exit(1);
         }
 
+        printf("\ncomando: %s \t resto:%s\n ", comando,corpo);
+        strcpy(msg.corpo,corpo);
+        strcpy(msg.comando,comando);
+        msg.pid = getpid();
+        if(write(fd_mngr_fifo,&msg,sizeof(msg)) == -1){
+            perror("Erro ao enviar mensagem para o servidor");
+            close(fd_mngr_fifo);
+            unlink(feedpipe_final);
+            exit(1);
+        }
+
+        
     }while(1);
 
 
