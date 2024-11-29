8902531d122db15731671c6fa9f704215f848335
MartimOli8 <132997229+MartimOli8@users.noreply.github.com>
2024-11-29 17:36:32 +0000

    Update feed.c

--- feed.c
+++ feed.c
@@ -8,79 +8,84 @@ int is_invalid_command(const char *comando) {
            strcmp(comando, "subscribe") != 0 &&
            strcmp(comando, "topics") != 0 &&
            strcmp(comando, "unsubscribe") != 0 &&
-           strcmp(comando, "exit") != 0;
-// verifica se o comando introduzido na consola é um comando válido
+           strcmp(comando, "exit") != 0 &&
+           strcmp(comando, "users") != 0;
 }
 
 int is_invalid_login(const char *comando, const char *corpo){
     return strcmp(comando,"login") == 0 && strcmp(corpo,"login");
 }
 
-
 int main(int argc, char * argv[])
 {
-    char comando[10],corpo[450];
+    char comando[10], corpo[450];
     message msg;
-    int fd_mngr_fifo,sizeM;
+    int fd_mngr_fifo, sizeM;
     usr user;
-    if(!argv[1]){
-        printf("Nome nao introduzido, por favor introduza um nome.\n");
+
+    if (!argv[1]) {
+        printf("Nome não introduzido, por favor introduza um nome.\n");
         exit(1);
     }
-    strcpy(user.nome_utilizador,argv[1]);
-    if(strlen(user.nome_utilizador) > 20){
+
+    strcpy(user.nome_utilizador, argv[1]);
+    if (strlen(user.nome_utilizador) > 20) {
         printf("\nNome demasiado grande, tente outra vez com um nome menor\n");
         exit(1);
     }
-    user.pid = getpid();
-    printf("%d, %s\n",user.pid, user.nome_utilizador);
-    //fazer aqui averificação de possibilidade de adicionar utilizador no manager
-    //melhor fazer com sinais, 3 situações: sucesso - falha por nome igual - falha por max
 
-    sprintf(feedpipe_final,"FEED_FIFO[%d]",getpid());
-    if(mkfifo(feedpipe_final,0666) == -1){
-        if(errno == EEXIST){
-            printf("named pipe com este pid ja existe\n");
+    user.pid = getpid();
+    printf("%d, %s\n", user.pid, user.nome_utilizador);
+
+    // Criando o pipe com o pid do feed
+    sprintf(feedpipe_final, "FEED_FIFO[%d]", getpid());
+    if (mkfifo(feedpipe_final, 0666) == -1) {
+        if (errno == EEXIST) {
+            printf("Named pipe com este pid já existe\n");
         }
-        printf("erro na criacao do named pipe\n");
+        printf("Erro na criação do named pipe\n");
         return 1;
     }
 
-    fd_mngr_fifo = open (ManPipe,O_WRONLY);
-        if (fd_mngr_fifo == -1){
-            printf("Erro a abrir o pipe do manager, ou pipe nao existe\n");
-            unlink(feedpipe_final);
-            return -1;
-        }
+    // Abrindo o pipe do manager para escrita
+    fd_mngr_fifo = open(ManPipe, O_WRONLY);
+    if (fd_mngr_fifo == -1) {
+        printf("Erro ao abrir o pipe do manager, ou pipe não existe\n");
+        unlink(feedpipe_final);
+        return -1;
+    }
 
-    do{
-
-        //lê inputs do user até ser introduzido um comando
+    do {
+        // Lê inputs do usuário até ser introduzido um comando válido
         do {
-            printf("\ninsira um comando valido:");
+            printf("\nInsira um comando válido: ");
             fgets(corpo, sizeof(corpo) - 1, stdin);
-            sscanf(corpo,"%s %[^\n]", comando, corpo);
-        }while(is_invalid_command(comando));
+            sscanf(corpo, "%s %[^\n]", comando, corpo);
+        } while (is_invalid_command(comando));
 
-        if(strcmp(comando,"exit") == 0){
+        if (strcmp(comando, "exit") == 0) {
             unlink(feedpipe_final);
             exit(1);
         }
 
-        printf("\ncomando: %s \t resto:%s\n ", comando,corpo);
-        strcpy(msg.corpo,corpo);
-        strcpy(msg.comando,comando);
+        // Print do que será enviado
+        printf("\nEnviando comando: %s\n", comando);
+        printf("Corpo da mensagem: %s\n", corpo);
+
+        // Copiar os dados para a mensagem
+        strcpy(msg.corpo, corpo);
+        strcpy(msg.comando, comando);
         msg.pid = getpid();
-        if(write(fd_mngr_fifo,&msg,sizeof(msg)) == -1){
+
+        // Enviar mensagem para o pipe
+        if (write(fd_mngr_fifo, &msg, sizeof(msg)) == -1) {
             perror("Erro ao enviar mensagem para o servidor");
             close(fd_mngr_fifo);
             unlink(feedpipe_final);
             exit(1);
         }
 
-        
-    }while(1);
-
+    } while (1);
 
     return 0;
 }
