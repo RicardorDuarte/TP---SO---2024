c73ff780bbbedd92483cc2142a708979abce8dd5
MartimOli8 <132997229+MartimOli8@users.noreply.github.com>
2024-11-29 17:39:27 +0000

    Create processocom.c

---
+++ processocom.c
@@ -0,0 +1,35 @@
+#include "processocom.h"
+
+void processa_comando(char *comando, man *manager) {
+    char username[20];
+    char topic[20];
+
+    if (strcmp(comando, "users") == 0) {
+        printf("users\n");
+        list_user(manager);  // Passe o endereço de 'manager' para as funções
+    }
+    else if (strcmp(comando, "remove") == 0) {
+        scanf("%s", username);
+        remover(manager, username);
+    }
+    else if (strcmp(comando, "topics") == 0) {
+        list_topics(manager);
+    }
+    else if (strcmp(comando, "show") == 0) {
+        scanf("%s", topic);
+        print_topic(manager, topic);
+    } else if (strcmp(comando, "lock") == 0) {
+        scanf("%s", topic);
+        lock_topic(manager, topic);
+    }
+    else if (strcmp(comando, "unlock") == 0) {
+        scanf("%s", topic);
+        unlock_topic(manager, topic);
+    }
+    else if (strcmp(comando, "close") == 0) {
+        exit(0); // Implementação de saída
+    }
+    else {
+        printf("Comando desconhecido\n");
+    }
+}
