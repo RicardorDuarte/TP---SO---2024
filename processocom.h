a6a59ac8cdb91f4892c84a251ddeb410389c41be
MartimOli8 <132997229+MartimOli8@users.noreply.github.com>
2024-11-29 17:39:48 +0000

    Create processocom.h

---
+++ processocom.h
@@ -0,0 +1,10 @@
+#ifndef PROCESSA_COMANDO_H
+#define PROCESSA_COMANDO_H
+#include "manager.h"
+#include "processoman.h"
+#include "utils.h"
+
+// Função que processa os comandos do usuário ou do pipe
+void processa_comando(char *comando, man *manager);
+
+#endif
