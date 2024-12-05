b798f6dded0caa1ee59c8f917ec52818270086ca
MartimOli8 <132997229+MartimOli8@users.noreply.github.com>
2024-12-05 16:56:51 +0000

    Update manager.h

--- Manager/manager.h
+++ Manager/manager.h
@@ -2,42 +2,5 @@
 #define MANAGER_H
 #include "processocom.h"
 
-typedef struct User usr, *pusr;
-struct User{
-	char nome_utilizador[20];
-	pid_t pid;
-};
-
-
-typedef struct Mensagem msg,*pmsg;
-struct Mensagem{
-    char comando[15];
-    char topico[15];
-    char corpo[450];
-    int duracao;
-    int npersistentes;//este n persistentes n pode ficar aqui, isto so recebe msgs
-	pid_t pid;
-};
-
-typedef struct Topic tp,*ptp;
-struct Topic{
-	char topico[20];
-	msg conteudo;
-    int lock;
-};
-
-typedef struct Manager man,*pman;
-struct Manager{
-    usr utilizador[MAXUSERS];
-    int nusers;
-    int ntopicos;
-    tp topicos[MAXTOPICS];
-};
-
-typedef struct {
-    int fd_manager_pipe;
-    man *manager;
-} PipeData;
-
 #endif
 
