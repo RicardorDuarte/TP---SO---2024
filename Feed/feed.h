16424e1551110568150695c9abf9e425f4c2ea5f
MartimOli8 <132997229+MartimOli8@users.noreply.github.com>
2024-12-05 16:55:26 +0000

    Update feed.h

--- Feed/feed.h
+++ Feed/feed.h
@@ -4,18 +4,3 @@
 int is_invalid_command(const char *comando);
 
 
-typedef struct User{
-    char nome_utilizador[20];
-    pid_t pid;
-}usr;
-
-typedef struct{
-    char comando[15];
-    char topico[15];
-    char corpo[450];
-    int duracao;
-    //int duracao;
-    //char texto[300];
-    pid_t pid;
-}message;
-
