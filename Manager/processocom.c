5ded186978edaecd7c7769df7b2f14f99c1dbed6
MartimOli8 <132997229+MartimOli8@users.noreply.github.com>
2024-12-05 20:14:51 +0000

    Update processocom.c

--- Manager/processocom.c
+++ Manager/processocom.c
@@ -33,39 +33,3 @@ void processa_comando_manager(char *comando, man *manager) {
     }
 }
 
-void * processa_comando_feed(char *nome, char *comando, void *manager) {
-    man *mngr = (man*)manager;
-    char username[20];
-    char topic[20];
-
-    if (strcmp(comando, "login") == 0) {
-        strcpy(mngr->utilizador[0].nome_utilizador,nome);
-        mngr->nusers++;        
-    }
-
-   else if (strcmp(comando, "users") == 0) {
-       scanf("%s", username);
-       list_user(manager);
-   }
-}
-//    else if (strcmp(comando, "topics") == 0) {
-//        list_topics(manager);
-//    }
-//    else if (strcmp(comando, "unsubscribe") == 0) {
-//        scanf("%s", topic);
-//        print_topic(manager, topic);
-//    } else if (strcmp(comando, "exit") == 0) {
-//        scanf("%s", topic);
-//        lock_topic(manager, topic);
-//    }
-//    else if (strcmp(comando, "users") == 0) {
-//        scanf("%s", topic);
-//        unlock_topic(manager, topic);
-//    }
-//    else if (strcmp(comando, "users") == 0) {
-//        exit(0); // Implementação de saída
-//    }
-//    else {
-//        printf("Comando desconhecido\n");
-//    }
-//}
