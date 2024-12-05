92ae154c1b2dfc350c3633edd4160cebc5e1d749
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-12-05 15:31:07 +0000

    Update processocom.c

--- processocom.c
+++ processocom.c
@@ -1,6 +1,6 @@
 #include "processocom.h"
 
-void processa_comando(char *comando, man *manager) {
+void processa_comando_manager(char *comando, man *manager) {
     char username[20];
     char topic[20];
 
@@ -33,3 +33,40 @@ void processa_comando(char *comando, man *manager) {
         printf("Comando desconhecido\n");
     }
 }
+
+void * processa_comando_feed(char *nome, char *comando, void *manager) {
+    man *mngr = (man*)manager;
+    //char username[20];
+    //char topic[20];
+
+    if (strcmp(comando, "login") == 0) {
+        printf("ola\n");
+        strcpy(mngr->utilizador[0].nome_utilizador,nome);
+    }
+    return NULL;
+}
+//    else if (strcmp(comando, "subscribe") == 0) {
+//        scanf("%s", username);
+//        remover(manager, username);
+//    }
+//    else if (strcmp(comando, "topics") == 0) {
+//        list_topics(manager);
+//    }
+//    else if (strcmp(comando, "unsubscribe") == 0) {
+//        scanf("%s", topic);
+//        print_topic(manager, topic);
+//    } else if (strcmp(comando, "exit") == 0) {
+//        scanf("%s", topic);
+//        lock_topic(manager, topic);
+//    }
+//    else if (strcmp(comando, "users") == 0) {
+//        scanf("%s", topic);
+//        unlock_topic(manager, topic);
+//    }
+//    else if (strcmp(comando, "users") == 0) {
+//        exit(0); // Implementação de saída
+//    }
+//    else {
+//        printf("Comando desconhecido\n");
+//    }
+//}
