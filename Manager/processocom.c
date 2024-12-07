918721f48dfbc2a6b3afa994e283f976f75f9050
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-12-07 22:56:17 +0000

    Update processocom.c
    ta a adicionar topicos mas so a cada user, falta adicionar ao array de topicos e incrementar o nº de topicos, mas agr n tou com cabeça para isso

--- Manager/processocom.c
+++ Manager/processocom.c
@@ -67,7 +67,7 @@ void * processa_comando_feed(char *corpo, char *comando, int pid, void *manager,
         }
     }
     
-    if (strcmp(comando,"users") == 0) {
+    if (strcmp(comando,"users") == 0) {    //apenas para testes!!!! este comando é so do manager!!!!!
         for (int i = 0; i < mngr->nusers && i < MAXUSERS; i++) {
                 printf("User %d: %s\n", i + 1, mngr->utilizadores[i].nome_utilizador);
         }
@@ -114,7 +114,6 @@ void * processa_comando_feed(char *corpo, char *comando, int pid, void *manager,
                         break;
                     }
                 }
-
                 // se nao, subscreve
                 if (!alr_subscribed) {
                     int add = 0;
@@ -136,14 +135,52 @@ void * processa_comando_feed(char *corpo, char *comando, int pid, void *manager,
                 break;
             }
         }
-
-        // user nao encontrado
-        if (!encontra) {
+        if (!encontra) { //so chega aqui se o managar for fechado e aberto e o feed continuar aberto (falta de login)
+                         //nao deve acontecer mas wtv
             printf("user com pid: %d nao encontrado.\n", pid);
         }
     }
-    return NULL;
-}
+    
+    if(strcmp(comando,"unsubscribe") == 0){
+        int findusr = 0;
+
+        // encontra user no array com PDI
+        for (int i = 0; i < mngr->nusers; i++) {
+            if (mngr->utilizadores[i].pid == pid) {
+                findusr = 1;
+
+                // verifica se esta subbed
+                int findtopic = 0;
+                for (int j = 0; j < MAXTOPICS; j++) {
+                    if (strcmp(mngr->utilizadores[i].subscrito[j], corpo) == 0) {
+                        findtopic = 1;
+
+                        // substitui o topico por string vazia
+                        mngr->utilizadores[i].subscrito[j][0] = '\0';
+                        printf("User '%s' subscreveu ao topico: '%s'.\n",mngr->utilizadores[i].nome_utilizador, corpo);
+
+                        // puxa os elementos depois do apagado para a esquerda
+                        for (int k = j; k < MAXTOPICS - 1; k++) {
+                            strcpy(mngr->utilizadores[i].subscrito[k], mngr->utilizadores[i].subscrito[k + 1]);
+                        }
+                        // limpa o ultimo depois de empurrar para evitar copias
+                        mngr->utilizadores[i].subscrito[MAXTOPICS - 1][0] = '\0';
+                        break;
+                    }
+                }
+                if (!findtopic) {
+                    printf("User '%s' nao esta subscrito no topico: '%s'.\n", mngr->utilizadores[i].nome_utilizador, corpo);
+                }
+                break;
+            }
+        }
+        if (!findusr) { //so chega aqui se o managar for fechado e aberto e o feed continuar aberto (falta de login)
+                        //nao deve acontecer mas wtv
+            printf("User %d nao encontrado, subscricao falhada\n", pid);
+        }
+    }
+
+    //foda-se esqueci-me que isto tem de ser feito no array de topicos, OH EGUA
     return NULL;
 }
 
