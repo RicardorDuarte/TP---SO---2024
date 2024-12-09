ebfed8875efc334d59ededa30a413f9d8e5a56f0
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-12-09 15:01:40 +0000

    possivel matar thread com sinal, sinal enviado com exit

--- Manager/manager.c
+++ Manager/manager.c
@@ -1,69 +1,76 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "manager.h"
 
 #define ManPipe "MANAGER_FIFO"
 #define FeedPipe "FEED_FIFO[%d]"
 char feedpipe_final[100];
 
-void *ler_pipe(void *pdata) {
-    PipeData *pipe_data = (PipeData *)pdata;
+void acorda() {}
+
+void *ler_pipe(void *tdata) {
+    TData *thread_data = (TData *)tdata;
     msg mensagemRecebida;
-    int fd_manager_pipe = pipe_data->fd_manager_pipe;
-    man *manager = pipe_data->manager;
+    int fd_manager_pipe = thread_data->fd_manager_pipe;
+    man *manager = thread_data->manager;
     usr user;
     int sizeMan;
     //char mensagemEnvia;
     
-    while (1) {
+    do{
         sizeMan = read(fd_manager_pipe, &mensagemRecebida, sizeof(mensagemRecebida));
 
         if (sizeMan > 0) {
             // Printf adicional para mostrar os dados recebidos
-            printf("Mensagem recebida do pipe:\n");
+            printf("\nMensagem recebida do pipe:\n");
             printf("Comando: %s\n", mensagemRecebida.comando);
             printf("Corpo: %s\n", mensagemRecebida.corpo);
             printf("PID: %d\n", mensagemRecebida.pid);
 
-            
+            pthread_mutex_lock(thread_data->m);
             processa_comando_feed(mensagemRecebida.corpo, mensagemRecebida.comando, mensagemRecebida.pid, manager, (void *)&user);
+            pthread_mutex_unlock(thread_data->m);
 
-
-
-            // Verifica se a mensagem "sair" foi recebida
-            if (strcmp(mensagemRecebida.corpo, "sair") == 0) {
-                close(fd_manager_pipe);
-                unlink(ManPipe);
-                exit(1);
-            }
-            else{
-                //mensagemEnvia=pipe_topics(manager);
-            }
-        } else {
-            perror("Erro ao ler do pipe");
-            break; // Encerra o loop em caso de erro.
+            
+            //mensagemEnvia=pipe_topics(manager);
         }
-	}
-    return NULL;
+         
+	}while(thread_data->trinco == 0);
+    close(fd_manager_pipe);
+    pthread_exit(NULL);
 }
+
+
 
 int main() {
     man manager;
-    pthread_t tid_pipe;
-    PipeData pipe_data;
+    pthread_t tid_pipe[1];
+    TData thread_data[1];
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex,NULL);
     char comando[20];
-    //char username[20];
-    //char topic[20];
+
     int fd_manager_pipe;
 
-    // Inicializa o manager
+    // Inicializacao do manager
     manager.nusers = 0;
     manager.ntopicos = 0;
+
+
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = acorda;
+    sigaction(SIGUSR1, &sa, NULL);
+
+
+    
 
     if (access(ManPipe, F_OK) == -1) {  // Verifica se o FIFO já existe
         if (mkfifo(ManPipe, 0666) == -1) {
             perror("Erro ao criar o FIFO");
             return -1;
         } else {
-            printf("FIFO '%s' criado com sucesso.\n", ManPipe);
+            printf("FIFO '%s' criado com sucesso.\n",ManPipe);
         }
     } else {
         printf("FIFO '%s' já existe. Ignorando criação.\n", ManPipe);
@@ -77,11 +84,12 @@ int main() {
     printf("FIFO '%s' aberto com sucesso.\n", ManPipe);
 
     // Prepara os dados para a thread ler o pipe
-    pipe_data.fd_manager_pipe = fd_manager_pipe;
-    pipe_data.manager = &manager;
+    thread_data[0].fd_manager_pipe = fd_manager_pipe;
+    thread_data[0].manager = &manager;
+    thread_data[0].m = &mutex;
 
     // Cria a thread para ler do pipe
-    if (pthread_create(&tid_pipe, NULL, ler_pipe, (void *)&pipe_data) != 0) {
+    if (pthread_create(&tid_pipe[0], NULL, ler_pipe, (void *)&thread_data[0]) != 0) {
         perror("Erro ao criar thread para ler do pipe");
         close(fd_manager_pipe);
         unlink(ManPipe);
@@ -89,18 +97,23 @@ int main() {
     }
 
     // Loop de interação com o usuário
-    do {
+    while(1){
         printf("CMD> ");
-        fflush(stdout);
         scanf("%s", comando);
+        if(strcmp(comando, "exit") == 0){
+            thread_data[0].trinco = 1;
+            pthread_kill(tid_pipe[0],SIGUSR1);
+            break;
+        }
         
         //// Processa o comando digitado pelo admin
         //processa_comando_manager(comando, &manager);
 
-    } while (strcmp(comando, "exit") != 0);
-    // fazer um sinal para quando exit enviar alguma coisa para desbloquear a thread
+    }
+
     // Espera a thread de leitura do pipe finalizar
-    pthread_join(tid_pipe, NULL);
+    pthread_join(tid_pipe[0], NULL);
+    pthread_mutex_destroy(&mutex);
 
     // Finaliza a execução
     close(fd_manager_pipe);
