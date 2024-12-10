44c6ef3f27e219d0d5fdc45ed9468910d5b976b9
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-12-10 20:09:31 +0000

    Update processocom.h

--- Manager/processocom.h
+++ Manager/processocom.h
@@ -2,5 +2,5 @@
 // Função que processa os comandos do usuário ou do pipe
 void processa_comando_manager(char *comando, man *manager);
 
-void *processa_comando_feed(char *corpo, char *comando, int pid, void *manager, void *users);
+void processa_comando_feed(msg *resposta,char *corpo, char *comando, int pid, void *manager, void *users);
 
