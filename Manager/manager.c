682b44780b0beaa30e1d0c90ceb72dc5d407e54a
RicardorDuarte <122127261+RicardorDuarte@users.noreply.github.com>
2024-12-10 20:08:33 +0000

    Update manager.c

--- Manager/manager.c
+++ Manager/manager.c
@@ -1,5 +1,6 @@
-#include "manager.h"
 #define _POSIX_C_SOURCE 200809L
+
+#include "manager.h"
 
 #define ManPipe "MANAGER_FIFO"
 #define FeedPipe "FEED_FIFO[%d]"
@@ -7,44 +8,55 @@ char feedpipe_final[100];
 
 void acorda() {}
 
-void *ler_pipe(void *pdata) {
+void *ler_pipe(void *tdata) {
     TData *thread_data = (TData *)tdata;
-    PipeData *pipe_data = (PipeData *)pdata;
-    msg mensagemRecebida;
-    int fd_manager_pipe = pipe_data->fd_manager_pipe, fd_feed_pipe;
-    man *manager = pipe_data->manager;
+
+    msg mensagemRecebida, mensagemEnvia;
+    man *manager = thread_data->manager;
     usr user;
+
+    int fd_manager_pipe = thread_data->fd_manager_pipe, fd_feed_pipe;
     int sizeMan;
-    msg mensagemEnvia;
+    
+
     mensagemEnvia.corpo[0] = '\0';
     mensagemEnvia.comando[0] = '\0';
     mensagemEnvia.duracao = 0;
     mensagemEnvia.npersistentes=0;
+
+
     do {
         sizeMan = read(fd_manager_pipe, &mensagemRecebida, sizeof(mensagemRecebida));
         if (sizeMan > 0) {
+
             printf("Mensagem recebida do pipe:\n");
             printf("Comando: %s\n", mensagemRecebida.comando);
             printf("Corpo: %s\n", mensagemRecebida.corpo);
             printf("PID: %d\n", mensagemRecebida.pid);
+
             mensagemEnvia.pid=mensagemRecebida.pid;
             pthread_mutex_lock(thread_data->m);
+
+
             if (strcmp(mensagemRecebida.corpo, "sair") == 0) {
                 close(fd_manager_pipe);
                 unlink(ManPipe);
                 exit(1);
             } else {
+
+
                 processa_comando_feed(&mensagemEnvia, mensagemRecebida.corpo, mensagemRecebida.comando, mensagemRecebida.pid, manager, (void *)&user);
                 strcpy(mensagemEnvia.comando, mensagemRecebida.comando);
                 printf("\n%s, %s", mensagemEnvia.comando,mensagemEnvia.corpo);
                 printf("CMD>");
-                fflush(stdout);
-                // Constrói o nome do FIFO do feed
+
+
+                // Constrói o nome do pipe de cada feed
                 sprintf(feedpipe_final, "FEED_FIFO[%d]", mensagemRecebida.pid);
 
-                // Verifica se o FIFO existe antes de abrir
+                // Verifica se o pipe existe antes de abrir
                 while (access(feedpipe_final, F_OK) == -1) {
-                    usleep(1000); // Espera 1 ms
+                    sleep(0.1); // Espera 1 ms
                 }
 
                 fd_feed_pipe = open(feedpipe_final, O_WRONLY);
@@ -61,7 +73,6 @@ void *ler_pipe(void *pdata) {
 
             pthread_mutex_unlock(thread_data->m);
         } else {
-            perror("Erro ao ler do pipe");
             break; // Encerra o loop em caso de erro.
         }
     }while(thread_data->trinco == 0);
@@ -76,20 +87,17 @@ int main() {
     pthread_mutex_t mutex;
     pthread_mutex_init(&mutex,NULL);
     char comando[20];
-
     int fd_manager_pipe;
 
-    // Inicializacao do manager
     manager.nusers = 0;
     manager.ntopicos = 0;
 
 
+    //setup sinal para matar a thread em caso do manager querer sair falta fechar os feeds
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = acorda;
     sigaction(SIGUSR1, &sa, NULL);
-
-
     
 
     if (access(ManPipe, F_OK) == -1) {  // Verifica se o FIFO já existe
@@ -103,7 +111,7 @@ int main() {
         printf("FIFO '%s' já existe. Ignorando criação.\n", ManPipe);
     }
 
-    fd_manager_pipe = open(ManPipe, O_RDWR);//aberto para leitura e escrita para nao ficar preso
+    fd_manager_pipe = open(ManPipe, O_RDWR); //aberto para leitura e escrita para nao ficar preso
     if (fd_manager_pipe == -1) {
         perror("Erro ao abrir o FIFO");
         return -1;
@@ -123,7 +131,7 @@ int main() {
         return -1;
     }
 
-    // Loop de interação com o usuário
+    // Loop de interação com o teclado do manager
     while(1){
         printf("CMD> ");
         scanf("%s", comando);
@@ -133,8 +141,11 @@ int main() {
             break;
         }
         
-        //// Processa o comando digitado pelo admin
-        //processa_comando_manager(comando, &manager);
+        // Processa comandos dados pelo admin
+        pthread_mutex_lock(thread_data->m);
+        processa_comando_manager(comando, &manager);
+        pthread_mutex_unlock(thread_data->m);
+
 
     }
 
